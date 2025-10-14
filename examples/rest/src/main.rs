b60203f6d31a4e1738ef1090543b9cc024403cc9
Jonathan Good <github@bungogood.com>
2025-10-14 19:58:57 +0100

    feat: ping pong rest example redirects

--- examples/rest/src/main.rs
+++ examples/rest/src/main.rs
@@ -2,8 +2,21 @@ use std::sync::Arc;
 
 use bytes::Bytes;
 use warp::Filter;
+use warp::Reply;
 
 use barge::Barge;
+use serde::Serialize;
+use warp::http::StatusCode;
+
+#[derive(Serialize)]
+struct Success<T> {
+    data: T,
+}
+
+#[derive(Serialize)]
+struct Problem {
+    leader: Option<String>,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -21,18 +34,56 @@ async fn main() -> anyhow::Result<()> {
         .and_then(|body: Bytes, barge: Arc<Barge>| async move {
             let data = body.to_vec();
             match barge.propose(data).await {
-                Ok(()) => Ok::<_, warp::Rejection>(warp::reply::with_status(
-                    "ok",
-                    warp::http::StatusCode::OK,
-                )),
-                Err(_) => Ok(warp::reply::with_status(
-                    "error",
-                    warp::http::StatusCode::INTERNAL_SERVER_ERROR,
-                )),
+                Ok(()) => {
+                    let resp = Success {
+                        data: "ok".to_string(),
+                    };
+                    let json = warp::reply::json(&resp);
+                    Ok::<_, warp::Rejection>(
+                        warp::reply::with_status(json, StatusCode::OK).into_response(),
+                    )
+                }
+                Err(_) => {
+                    // TODO: query actual leader from `barge`; placeholder for now
+                    let leader_hint = "http://127.0.0.1:3030/ping".to_string();
+                    let prob = Problem {
+                        leader: Some(leader_hint.clone()),
+                    };
+                    let reply = warp::reply::json(&prob);
+                    let reply = warp::reply::with_status(reply, StatusCode::CONFLICT);
+                    let reply = warp::reply::with_header(reply, "x-leader", leader_hint);
+                    Ok::<_, warp::Rejection>(reply.into_response())
+                }
             }
         });
 
-    let routes = propose;
+    let test = warp::path("ping")
+        .and(warp::post())
+        .and(barge_filter.clone())
+        .and_then(|_barge: Arc<Barge>| async move {
+            let resp = Success {
+                data: "ping".to_string(),
+            };
+            let json = warp::reply::json(&resp);
+            let reply = warp::reply::with_status(json, StatusCode::OK);
+            Ok::<_, warp::Rejection>(reply.into_response())
+        });
+
+    let pong = warp::path("pong")
+        .and(warp::post())
+        .and(barge_filter.clone())
+        .and_then(|_barge: Arc<Barge>| async move {
+            let leader_hint = "http://127.0.0.1:3030/ping".to_string();
+            let prob = Problem {
+                leader: Some(leader_hint.clone()),
+            };
+            let json = warp::reply::json(&prob);
+            let reply = warp::reply::with_status(json, StatusCode::CONFLICT);
+            let reply = warp::reply::with_header(reply, "x-leader", leader_hint);
+            Ok::<_, warp::Rejection>(reply.into_response())
+        });
+
+    let routes = propose.or(test).or(pong);
 
     println!("Listening on http://127.0.0.1:3030");
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
